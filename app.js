a567539b6bce6053d1cce12b83488004b831343b
edenrstapleton <102817586+edenrstapleton@users.noreply.github.com>
2022-04-20 13:09:24 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    
+    if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper' || userInput === 'bomb') {
+      return userInput
+    }else{
+    console.log('Error, Please select Rock, Paper, Or Scissors ')
+    }
+    };
+    
+    const getComputerChoice = () => {
+      const randomNumber = (Math.floor(Math.random() * 3));
+      switch (randomNumber) {
+        case 0: 
+        return 'rock';
+        case 1: 
+        return 'paper';
+        case 2: 
+        return 'scissors';
+      }
+    };
+    
+    const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'This game is a tie!';
+    }
+    if (userChoice === 'rock') {
+    if (computerChoice === 'paper'){
+      return "Computer Won!";
+    } else { 
+      return "congratulations, you won!";
+    }
+    }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors'){
+        return "Computer Won!";
+      } else {
+        return "congratulations you won!";
+      }
+      if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+          return "Computer Won!"
+        } else { 
+          return "congratulations you won!"
+        }
+      }
+    }
+    if (userChoice === 'bomb') {
+      return 'congratulations you won!'
+    }
+    };
+    
+    console.log(determineWinner('scissors', 'rock'));
+    console.log(determineWinner('paper' , 'scissors'));
+    console.log(determineWinner('rock', 'rock'));
+    
+    
+    const playGame = () => {
+      const userChoice = getUserChoice('rock');
+      const  computerChoice = getComputerChoice();
+      console.log('You threw: ' + userChoice );
+      console.log('Computer threw: ' + computerChoice);
+    
+      console.log(determineWinner(userChoice, computerChoice));
+    };
+    
+    playGame();
